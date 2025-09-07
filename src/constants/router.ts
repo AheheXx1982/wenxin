e3303a92f300825758dfd21678a0f77fb3aad380
AheheXx1982 <ahehexx1982@gmail.com>
2025-09-07 20:47:58 +0800

    幻念集

--- src/constants/router.ts
+++ src/constants/router.ts
@@ -7,7 +7,7 @@ export type Router = {
 
 export enum Routes {
   Home = '/',
-  Investment = '/investment',
+  IllusionaryThoughts = '/illusionary-thoughts',
   About = '/about',
   Categories = '/categories',
   Tags = '/tags',
@@ -28,7 +28,7 @@ export const routers: Router[] = [
       { name: '归档', path: Routes.Archives, icon: 'ri:archive-2-fill' },
     ],
   },
-  { name: '投资分享', path: Routes.Investment, icon: 'ri:stock-line' },
+  { name: '幻念集', path: Routes.IllusionaryThoughts, icon: 'ri:stock-line' },
   { name: '关于', path: Routes.About, icon: 'fa6-regular:circle-user' },
   // { name: '展示柜', path: Routes.Gallery },
   // { name: '仪表盘', path: Routes.Dashboard, needOwner: true },
