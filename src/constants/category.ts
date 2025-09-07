30f63309baba6982ea75fb71b4d7c6c0ddda7b3c
AheheXx1982 <ahehexx1982@gmail.com>
2025-09-07 17:07:46 +0800

    仲夏夜之梦

--- src/constants/category.ts
+++
@@ -1,6 +0,0 @@
-import transformShokaConfig from '@scripts/transformShokaConfig';
-
-const shokaConfig = transformShokaConfig();
-
-// { '随笔': 'life' }
-export const categoryMap: { [name: string]: string } = shokaConfig?.categoryMap || {};
