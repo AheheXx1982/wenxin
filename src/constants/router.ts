fa1badc7ad55b647bbd00d8747c74e51e5a2c3c4
AheheXx1982 <ahehexx1982@gmail.com>
2025-09-09 02:16:56 +0800

    Update router.ts

--- src/constants/router.ts
+++ src/constants/router.ts
@@ -40,7 +40,7 @@ export const routers: Router[] = [
   // 将课程、实盘、自述、幻念集作为同级目录菜单
   { name: '课程', path: Routes.OptionsCourse, icon: 'ri:book-read-fill' },
   { name: '实盘', path: Routes.TradingJournal, icon: 'ri:stock-line' },
-  { name: '自述', path: Routes.About, icon: 'fa6-regular:circle-user' },
+  { name: '关于', path: Routes.About, icon: 'fa6-regular:circle-user' },
   { name: '幻念集', path: Routes.IllusionaryThoughts, icon: 'ri:stock-line' },
   // { name: '展示柜', path: Routes.Gallery },
   // { name: '仪表盘', path: Routes.Dashboard, needOwner: true },
