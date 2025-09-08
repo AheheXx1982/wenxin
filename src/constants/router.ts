6e4289f9155e853fe4c7ae69404d4a6e26645957
AheheXx1982 <ahehexx1982@gmail.com>
2025-09-09 00:49:59 +0800

    幻念集3.0

--- src/constants/router.ts
+++ src/constants/router.ts
@@ -7,7 +7,7 @@ export type Router = {
 
 export enum Routes {
   Home = '/',
-  Articles = '/articles',
+  // Articles = '/articles',
   IllusionaryThoughts = '/illusionary-thoughts',
   About = '/about',
   Categories = '/categories',
@@ -23,19 +23,24 @@ export enum Routes {
 
 export const routers: Router[] = [
   { name: '首页', path: Routes.Home, icon: 'fa6-solid:house-chimney' },
-  {
-    name: '猎场',
-    path: Routes.Articles,
-    icon: 'ri:quill-pen-ai-fill',
-    children: [
-      // { name: '标签', path: Routes.Tags, icon: 'fa6-solid:tags' },
-      // { name: '分类', path: Routes.Categories, icon: 'ri:grid-fill' },
-      // { name: '归档', path: Routes.Archives, icon: 'ri:archive-2-fill' },
-      { name: '课程', path: Routes.OptionsCourse, icon: 'ri:book-read-fill' },
-      { name: '实盘', path: Routes.TradingJournal, icon: 'ri:stock-line' },
-      { name: '自述', path: Routes.About, icon: 'fa6-regular:circle-user' },
-    ],
-  },
+  // 移除猎场分类
+  // {
+  //   name: '猎场',
+  //   path: Routes.Articles,
+  //   icon: 'ri:quill-pen-ai-fill',
+  //   children: [
+  //     // { name: '标签', path: Routes.Tags, icon: 'fa6-solid:tags' },
+  //     // { name: '分类', path: Routes.Categories, icon: 'ri:grid-fill' },
+  //     // { name: '归档', path: Routes.Archives, icon: 'ri:archive-2-fill' },
+  //     { name: '课程', path: Routes.OptionsCourse, icon: 'ri:book-read-fill' },
+  //     { name: '实盘', path: Routes.TradingJournal, icon: 'ri:stock-line' },
+  //     { name: '自述', path: Routes.About, icon: 'fa6-regular:circle-user' },
+  //   ],
+  // },
+  // 将课程、实盘、自述、幻念集作为同级目录菜单
+  { name: '课程', path: Routes.OptionsCourse, icon: 'ri:book-read-fill' },
+  { name: '实盘', path: Routes.TradingJournal, icon: 'ri:stock-line' },
+  { name: '自述', path: Routes.About, icon: 'fa6-regular:circle-user' },
   { name: '幻念集', path: Routes.IllusionaryThoughts, icon: 'ri:stock-line' },
   // { name: '展示柜', path: Routes.Gallery },
   // { name: '仪表盘', path: Routes.Dashboard, needOwner: true },
